78c0d070bb412d5ec64f882bc24a14ebd77789f3
MayPyone <maypyone015@gmail.com>
2025-01-17 18:52:23 +0630

    Update db connection

--- server/db.js
+++ server/db.js
@@ -4,7 +4,6 @@ const connect = new Pool({
     user: process.env.USER,
     password: process.env.PASSWORD,
     host: process.env.HOST,
-    port :process.env.PORT,
     database: process.env.DATABASE,
     database_url: process.env.Database_URL
 })
