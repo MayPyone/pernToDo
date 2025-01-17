4fee1b1e889997e4674696da336d76401a1ba355
MayPyone <maypyone015@gmail.com>
2025-01-17 19:37:26 +0630

    Add db initialization

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const Pool = require("pg").Pool;
 
-const connect = new Pool({
+const pool = new Pool({
     user: process.env.USER,
     password: process.env.PASSWORD,
     host: process.env.HOST,
@@ -8,4 +8,19 @@ const connect = new Pool({
     database_url: process.env.Database_URL
 })
 
-module.exports = connect
+const initializeDatabase = async () => {
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS todo (
+                id SERIAL PRIMARY KEY,
+                description TEXT NOT NULL,
+                completed BOOLEAN DEFAULT false
+            );
+        `);
+        console.log('Database initialized');
+    } catch (error) {
+        console.error('Error initializing database:', error);
+    }
+};
+
+module.exports = {pool, initializeDatabase}
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const pool = require('./db')
+const {pool, initializeDatabase} = require('./db')
 const router = require('./routes')
 
 //middlewares
@@ -10,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 const startUp = async()=> {
+    await initializeDatabase()
     await pool.connect()
     console.log("connected to db")
 }
@@ -21,7 +22,7 @@ app.get('/',async(req,res)=> {
 
 startUp()
 
-const port = process.env.SERVER_PORT || 5000
+const port = process.env.PORT || 5000
 app.listen(port,()=> {
     console.log("server is starting on port ",port)
 })
