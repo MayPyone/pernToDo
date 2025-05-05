af2073c666d0e36157d518794b6c3a1bb682674d
May Pyone <maypyone015@gmail.com>
2025-05-05 23:45:29 +0630

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+const pool = process.env.NODE_ENV === "production"
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    })
+  : new Pool({
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+      host: process.env.HOST,
+      database: process.env.DATABASE,
+      port: 5000
+    });
+
 
 const initializeDatabase = async () => {
     try {
